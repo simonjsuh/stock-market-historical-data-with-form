71af27458fd41accde2c22b653ef91b5748aeec8
simonjsuh <simon.j.suh@gmail.com>
2019-08-29 18:16:19 -0700

    Updates

---
+++ precache-manifest.295a78d4144ef97235596cdd44c6bdd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5c41808cef3fa75c23dc0293d59c0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9657121a67894d3c311",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "02c6138691b9b6aedee0",
+    "url": "/static/js/2.2242db9b.chunk.js"
+  },
+  {
+    "revision": "d9657121a67894d3c311",
+    "url": "/static/js/main.0902b448.chunk.js"
+  },
+  {
+    "revision": "01cd6b26d589a96faa72",
+    "url": "/static/js/runtime~main.f9a3736e.js"
+  }
+]);
